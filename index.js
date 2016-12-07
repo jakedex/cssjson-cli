00c3087127f0879c9291f80d476b78c17c437ff0
Jake Dexheimer <jakedex@gmail.com>
2016-12-06 18:56:12 -0600

    Implement streams, basic functionality working

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+const through2 = require('through2')
+const cssjson = require('cssjson')
 const argv = require('yargs')
-  .usage('Usage: $0 [--to-css] inputFile [--output|-o outputFile]')
+  .usage('Usage: $0 inputFile [--to-css] [--output|-o outputFile]')
   .example('cssjson input.css -o output.json', 'Convert css to json')
-  .example('cssjson --to-css input.js -o output.css', 'Convert json to css')
+  .example('cssjson input.js --to-css -o output.css', 'Convert json to css')
   .demand(1)
   .describe('to-css', 'Convert json input to css output')
   .alias('o', 'output')
@@ -13,5 +16,23 @@ const argv = require('yargs')
   .alias('h', 'help')
   .argv
 
-// const fs = require('fs')
-// const s = fs.createReadStream(argv.)
+const toCSS = argv['to-css']
+const inputStream = fs.createReadStream(argv._[0])
+var outputStream = argv.output ? fs.createWriteStream(argv.output) : process.stdout
+
+var _buffer = []
+var convertStream = through2(function (chunk, enc, callback) {
+  _buffer.push(chunk.toString('utf8'))
+  callback()
+}, function (callback) {
+  var data = _buffer.join('')
+  var convertedObj = toCSS ? cssjson.toCSS(JSON.parse(data)) : JSON.stringify(cssjson.toJSON(data))
+  // var converted = new Buffer(convertedObj)
+  this.push(convertedObj)
+  callback()
+})
+
+// pipe it!
+inputStream
+  .pipe(convertStream)
+  .pipe(outputStream)
